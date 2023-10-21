c2556a0fdddc04961a077829d308dba51189d58b
gonzolog <benwelly4@gmail.com>
2023-10-21 20:35:05 +0100

    Added factory function to create mutliple movies with additional properties. Also added the movies I created to an array for later use so that array can be itterated through

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 // JavaScript program that recommends a movie based on emotion, current weather and age of audience
 
+const create_movie = (title, emotion, weather, age_restriction) => {
+    return {
+        title,
+        emotion, // Expected input: "Scared", "Happy", "Excited" or "Sad"
+        weather, // Expected input: "Sun", "Rain" or "Snow"
+        age_restriction // Expected input: Whole number greater than 0 
+    }
+};
 
-// Function that takes three arguments (mood, current_weather, age_of_audience)
-// Exepcted input:
-    //Emotion (How the user wants to feel watching the film):   
-        //Scared", "Happy", "Excited", "Sad"
-    //Current Weather (How the weather outside effects what the user should watch)
-        //"Sun", "Rain", "Snow", "Dry"
-    // Age of Audience (Relevant to the age restrictions of the movie suggestion)
-        // Any whole number input from 1 - 18+
-const suggest_movie = (mood, current_weather, age_of_audience) => {
+// Using object factory to generate movies
+    // Horrors
+const the_shining = create_movie("The Shining", "scared", "snow", 15);
+const the_ring = create_movie("The Ring", "scared", "rain", 15);
+const midsommar = create_movie("Midsommar", "scared", "sun", 18);
+    // Feel-Good
+const groundhog_day = create_movie("Groundhog Day", "happy", "snow", 8);
+const harry_potter = create_movie("Harry Potter", "happy", "rain", 8);
+const mamma_mia = create_movie("Mamma Mia", "happy", "sun", 8);
+    // Action
+const everest = create_movie("Everest", "excited", "snow", 12)
+const the_dark_knight = create_movie("The Dark Knight", "excited", "rain", 12)
+const mission_impossible = create_movie("Mission Impossible", "excited", "sun", 12)
+    // Depressing
+const chronicles_of_narnia = create_movie("The Chronicles of Narnia", "sad", "snow", 8)
+const schindlers_list = create_movie("Schindler's List", "sad", "rain", 15)
+const a_man_called_otto = create_movie("A Man Called Otto", "sad", "sun", 15)
 
-}
-
-// Example usage:
-    // suggest_movie(scary)
-    // output: "The Descent"
+// Array containing every movies
+movie_list = [the_shining, the_ring, midsommar, groundhog_day, har, mamma_mia, everest, the_dark_knight, mission_impossible, chronicles_of_narnia, schindlers_list, a_man_called_otto]
