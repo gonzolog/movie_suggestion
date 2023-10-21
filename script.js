0bf0d90d8855fcd049e727219b08e73d7695b626
gonzolog <benwelly4@gmail.com>
2023-10-21 20:35:05 +0100

    Added factory function to create mutliple movies with additional properties. Also added the movies I created to an array for later use so that array can be itterated through

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 // JavaScript program that recommends a movie based on emotion, current weather and age of audience
 
-
-// Function that takes three arguments (mood, current_weather, age_of_audience)
-// Exepcted input:
-    //Emotion (How the user wants to feel watching the film):   
-        //Scared", "Happy", "Excited", "Sad"
-    //Current Weather (How the weather outside effects what the user should watch)
-        //"Sun", "Rain", "Snow", "Dry"
-    // Age of Audience (Relevant to the age restrictions of the movie suggestion)
-        // Any whole number input from 1 - 18+
-const suggest_movie = (mood, current_weather, age_of_audience) => {
-
+const create_movie = (emotion, weather, age_restriction) => {
+    return {
+        emotion, // "Scared", "Happy", "Excited", "Sad"
+        weather, // "Sun", "Rain", "Snow", "Dry"
+        age_restriction // Any whole number input from 1 - 18+
+    }
 }
 
-// Example usage:
-    // suggest_movie(scary)
-    // output: "The Descent"
+const the_shining = create_movie("scared", "snow", 15)
+console.log(the_shining)
+console.log("hello")
