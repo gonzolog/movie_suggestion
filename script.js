404b1ef6111a2ed9efd37fb7268e0e9a8b617566
gonzolog <benwelly4@gmail.com>
2023-10-21 12:13:24 +0100

    Added intial desrcriptive comments for function that lay the foundations for how I expect it to work

---
+++ script.js
@@ -0,0 +1,18 @@
+// JavaScript program that recommends a movie based on emotion, current weather and age of audience
+
+
+// Function that takes three arguments (mood, current_weather, age_of_audience)
+// Exepcted input:
+    //Emotion (How the user wants to feel watching the film):   
+        //Scared", "Happy", "Excited", "Sad"
+    //Current Weather (How the weather outside effects what the user should watch)
+        //"Sun", "Rain", "Snow", "Dry"
+    // Age of Audience (Relevant to the age restrictions of the movie suggestion)
+        // Any whole number input from 1 - 18+
+const suggest_movie = (mood, current_weather, age_of_audience) => {
+
+}
+
+// Example usage:
+    // suggest_movie(scary)
+    // output: "The Shining"
